da5198331f3b3e823d3bbe1cf3c69ca27a1e31c9
Rod Mastria <rodmastria@gmail.com>
2024-02-21 15:07:49 -0500

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace CoffeeShopRegistration.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public bool topTenCustomer { get; set; }
+
+        public void setCustomer(bool topCustomer)
+        {
+            this.topTenCustomer = topCustomer;
+        }
+    }
+}
